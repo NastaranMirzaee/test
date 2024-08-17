3e28c7be666a352b84f69608f0445a0b7b86c5d2
Nastaran <nstrnm4381@gmail.com>
2024-08-18 01:33:36 +0330

    add 2 comment line to the files

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+        // this is another comment line for the test git
     }
 }
