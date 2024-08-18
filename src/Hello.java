2a0c4ef5a2942c5019fb80ec17d1b0591a5474e7
Nastaran <nstrnm4381@gmail.com>
2024-08-18 14:09:17 +0330

    add commit about test new branch

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // this is another comment line for the test git
+        // I want to test the another branch in git
     }
 }
