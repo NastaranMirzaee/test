f5effb9866ade8caef3a7ff6118efa1e2bce104d
Nastaran <nstrnm4381@gmail.com>
2024-08-20 16:51:35 +0330

    add hello wordl again

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,12 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        // this is another comment line for the test git
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 }
